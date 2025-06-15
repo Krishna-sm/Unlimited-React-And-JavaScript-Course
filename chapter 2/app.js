14454b3ae91113ba2ff00267ae8b19d3e1ea52a0
Krishna-sm <krishna.web@gmail.com>
2025-06-15 17:44:46 +0530

    Chapter 2

---
+++ chapter 2/app.js
@@ -0,0 +1,113 @@
+// functions
+// radheyRadhey()
+// function radheyRadhey(){
+
+//     console.log("Radhey Radhey")
+// }
+
+// radheyRadhey()
+// radheyRadhey()
+// radheyRadhey()
+// radheyRadhey()
+// radheyRadhey()
+// radheyRadhey()
+// radheyRadhey()
+
+// first signautrue const keyword 
+
+// const radheyRadhey = function(){
+//      console.log("Radhey Radhey")
+// }
+
+// radheyRadhey()
+
+// const radheyRadhey = ()=>{
+//      console.log("Radhey Radhey")
+// }
+
+// radheyRadhey()
+
+
+// write a function to add 2 no dynamically 
+// formal parameter
+
+
+// /**
+//  * @param {Number} a
+//  * @param {Number} b
+//  * @returns {Number}
+//  */
+// const sum = (a=1,b)=>{
+//     // let a= 10
+//     // let b= 20 
+//     console.log(a+b)
+// }
+// actual paramter
+// sum(10,20)
+// sum(70,10)
+// sum(60,40)
+// sum(90,60)
+// sum(10)
+// sum(10,12)
+
+
+// props
+
+// const sum = (prop)=>{
+
+//     const {a=1,b=1} = prop
+
+//     console.log(a+b)
+// }
+
+// sum({a:1,b:2})
+// sum({b:1})
+// sum({a:11})
+
+
+// import {sub,sum,PI} from './calculator.js'
+
+// // console.log(Calculator)
+// sub(10,12)
+// sum(10,12)
+// console.log(PI);
+
+// Calculator.sum(10,12)
+// Calculator.sub(10,12)
+// Calculator(10,20)
+// Calculator[0](10,20)
+// Calculator[1](10,20)
+
+
+// return statement 
+
+
+// const square=(x)=>{
+//     return x**2
+//     ddsadsada
+// }
+// const square=function(x){
+//     return x**2
+//     ddsadsada
+// }
+
+// const square = (x)=>x**2
+
+// let result = square(2)
+
+// console.log(result);
+
+
+// let str ="123456"
+
+// console.log(parseInt(str)+2);
+
+
+// sign 2: Every Function Name in Module is Named as a File Name
+// sign 3: Every Module name start with Capital Letter
+// import Navbar from "./Navbar.js";
+
+// console.log(Navbar());
+
+
+{/* <Navbar/> */}
